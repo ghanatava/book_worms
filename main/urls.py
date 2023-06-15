a1f7ea56b21ae6525fb4cff457fe1dc2fe1da97c
ashura <ghanatva@gmail.com>
2023-06-15 07:21:33 +0530

    productlist added

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('about_us/',views.about,name='about'),
-    path('contact_us/',views.ContactUsView.as_view(),name='contact'),
+    path('contact_us/', views.ContactUsView.as_view(), name='contact'),
+    path('products/<slug:tag>',views.product_list,name='products')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic.edit import FormView
-from main import forms
+from main import forms,models
 
 # Create your views here.
 def index(request):
@@ -17,3 +17,14 @@ class ContactUsView(FormView):
     def form_valid(self,form):
         form.send_mail()
         return super().form_valid(form)
+
+def product_list(request,tag):
+    if tag != 'all':
+        tag = get_object_or_404(models.ProductTag, slug=tag)
+
+    if tag and tag != 'all':
+        products=models.Product.objects.active().filter(tags=tag)
+    else:
+        products = models.Product.objects.active()
+        
+    return render(request,'main/product_list.html',{'products':products})
